2dc49b1e39c98966979680e6a09ef2df37afcc9c
Gabbinetto <gabrieleferrante97@googlemail.com>
2025-07-17 12:16:39 +0200

    Rimosso booleano inutile

--- sql/data_types.sql
+++ sql/data_types.sql
@@ -1,5 +1,5 @@
 CREATE TYPE NOME_BACHECA AS ENUM ('Università', 'Lavoro', 'Tempo libero');
 CREATE DOMAIN NOME_UTENTE AS VARCHAR(16) CHECK
-    (CHAR_LENGTH(value) > 2 AND value ~* '^[a-zA-Z0-9]{3,16}$');
+    (value ~* '^[a-zA-Z0-9]{3,16}$');
 CREATE DOMAIN DOM_PASSWORD AS VARCHAR(16) CHECK
-    (CHAR_LENGTH(value) > 6 AND value ~* '^[a-zA-Z0-9._?!/@*]{6,16}$');
+    (value ~* '^[a-zA-Z0-9._?!/@*]{6,16}$');
